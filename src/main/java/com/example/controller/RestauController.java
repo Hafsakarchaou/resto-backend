813cc1a97bc63d389a92cf5e550b38edcf61a88d
Hafsakarchaou <hafsadiy@gmail.com>
2023-06-03 18:34:19 +0200

    make it better

--- src/main/java/com/example/controller/RestauController.java
+++ src/main/java/com/example/controller/RestauController.java
@@ -8,15 +8,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.entities.Photo;
 import com.example.entities.Restaurant;
 import com.example.service.RestauService;
-@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin("*")
 @RestController
 @RequestMapping("restaurants")
 public class RestauController {
@@ -39,7 +38,7 @@ public class RestauController {
 	}
 	
 	
-	@GetMapping("/update")
+	@PutMapping("/update/{id}")
 	public void update(@PathVariable String id,@RequestBody Restaurant restaurant){
 		restoService.update(id, restaurant);
 	}
